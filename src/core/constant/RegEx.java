c3fb2568491475b4faff7f2c0521ce64b516f541
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-14 19:20:58 +0700

    add comment

--- src/core/constant/RegEx.java
+++ src/core/constant/RegEx.java
@@ -6,6 +6,7 @@
 package core.constant;
 
 public class RegEx {
+    // reusability
     public static final String NAME = "[A-Za-z]+";
     public static final String EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 }
