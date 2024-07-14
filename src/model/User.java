94a7786ab55c4c541d4cb832dd6b43f0ac5b757a
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-14 19:00:56 +0700

    add User model

---
+++ src/model/User.java
@@ -0,0 +1,43 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+package model;
+
+public class User {
+    private String name;
+    private String email;
+
+    private int age;
+
+    public User(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
