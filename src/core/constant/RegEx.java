65a4009024602555e6fbed7466154600fad6ce83
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-14 19:10:46 +0700

    add RegEx class

---
+++ src/core/constant/RegEx.java
@@ -0,0 +1,11 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+package core.constant;
+
+public class RegEx {
+    public static final String NAME = "[A-Za-z]+";
+    public static final String EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+}
